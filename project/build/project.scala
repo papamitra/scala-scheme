978f31aec546e2a81740e84087929810582bbb14
papamitra <papamitra@gmail.com>
2010-10-09 11:51:07 +0900

    use scalatest 1.0

--- project/build/project.scala
+++ project/build/project.scala
@@ -2,6 +2,6 @@
 import sbt._
 
 class Project(info:ProjectInfo) extends DefaultProject(info){
-  val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final"
+  val scalatest = "org.scalatest" % "scalatest" % "1.0"
 }
 
