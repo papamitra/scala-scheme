5a98a97fec7f7a75fa3c2d0f2336cfb5200ebcc7
papamitra <papamitra@gmail.com>
2010-10-09 17:50:53 +0900

    ()の対応間違えてました…

--- src/test/scala/PrimitiveTest.scala
+++ src/test/scala/PrimitiveTest.scala
@@ -19,13 +19,13 @@ class PrimitiveTestSuite extends FunSuite{
   }
 
   test("isNull"){
-    assert(isNull(parse("()")) == parse("#t"))
-    assert(isNull(parse("(1 2 3)")) == parse("#f"))
-    assert(isNull(parse("'a")) == parse("#f"))
-    assert(isNull(parse("0")) == parse("#f"))
+    assert(isNull(parse("()")) === parse("#t"))
+    assert(isNull(parse("(1 2 3)")) === parse("#f"))
+    assert(isNull(parse("'a")) === parse("#f"))
+    assert(isNull(parse("0")) === parse("#f"))
   }
 
   test("car"){
-    assert(car(parse("(1 2 3)") == parse("1")))
+    assert(car(parse("(1 2 3)")) === parse("1"))
   }
 }
