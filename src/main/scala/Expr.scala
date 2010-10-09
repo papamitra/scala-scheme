3e129f3789b06ead6c6fcaeb8deb39a69c3d0e13
前田 康行 <maeda@Maedas-MacBook-Air.local>
2010-10-09 13:49:32 +0900

    Add toString to Expr

--- src/main/scala/Expr.scala
+++ src/main/scala/Expr.scala
@@ -2,7 +2,15 @@
 package org.nagoyahackathon.scalalisp
 
 sealed class Expr
-case class Symbol(sym:String) extends Expr
-case class ListExpr(lst:List[Expr]) extends Expr
-case class StringExpr(str:String) extends Expr
-case class Number(num:Int) extends Expr
+case class Symbol(sym:String) extends Expr{
+  override def toString = "'%s" format sym
+}
+case class ListExpr(lst:List[Expr]) extends Expr{
+  override def toString = lst map {_.toString} mkString("(", ", ", ")")
+}
+case class StringExpr(str:String) extends Expr{
+  override def toString = "\"%s\"" format str
+}
+case class Number(num:Int) extends Expr{
+  override def toString = num.toString
+}
